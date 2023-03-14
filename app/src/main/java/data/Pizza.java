9e15cf99297cad72678c1cd75606f37d9e022cf8
Ken Ulysses <cadungogken.05@gmail.com>
2023-03-14 11:01:34 +0800

    PIzzaFragments Elect2 1:30_6:30

---
+++ app/src/main/java/data/Pizza.java
@@ -0,0 +1,19 @@
+package data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pizza {
+
+        public static String[] Menus = {
+                "Hotdog Pizza",
+                "Cheese and Pepper Pizza",
+                "Garlic Pizza"
+        };
+
+        public static String[] Details = {
+                "Premium Hotdog Toppings around the crust and with hot sauce",
+                "Cheese and Pepper all the way"
+        };
+}
